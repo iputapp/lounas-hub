0abad455c682a2f8880b6e94cd892842a37cb132
wiyco <72733890+wiyco@users.noreply.github.com>
2023-12-07 20:49:26 +0900

    [Font]: Add font settings to the root layout
    - Font: Noto Sans JP

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,24 @@
-import type { Metadata } from "next";
-import { Providers } from "./providers";
 import "./globals.scss";
+
+import type { Metadata } from "next";
+import { Noto_Sans_JP } from "next/font/google";
+
+import { Providers } from "./providers";
 
 export const metadata: Metadata = {
   title: "lounas-hub",
-  description: "The admin tool of lounas",
+  description: "lounasの管理ツールです。",
 };
+
+const notoSansJP = Noto_Sans_JP({
+  subsets: ["latin"],
+  display: "swap",
+});
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="ja">
-      <body>
+      <body className={notoSansJP.className}>
         <Providers>{children}</Providers>
       </body>
     </html>
